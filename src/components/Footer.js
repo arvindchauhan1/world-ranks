86e9b78642af79094145e93ebba22abbb30ac3d0
Arvind Chauhan <psych3080@gmail.com>
2022-03-20 15:39:59 +0530

    fixed css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div class="fixed bottom-0 w-full text-center bg-slate-50 h-9 pt-2 text-sm">
+        <div className="fixed bottom-0 w-full text-center bg-slate-100 h-9 pt-2 text-sm">
             © psych3080
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return <>
-  <div className="sticky top-0 text-center py-5 bg-slate-50">
+  <div className="sticky top-0 text-center py-5 bg-slate-100">
       World Ranks
   </div>
   </>;
